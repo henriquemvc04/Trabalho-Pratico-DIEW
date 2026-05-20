da661b44497be3cd4bc522987883ea52bfe59319
Henrique Shevchenko <henriqueshevchenko4527@gmail.com>
2026-05-20 14:47:53 -0300

    Um pouco das ligas e times

--- index.js
+++ index.js
@@ -1,41 +1,192 @@
-const ligas =
-{
-    "imagens":[
-        {
-            "id": 1,
-            "nome": "Premier League",
-            "imagem": "imgsLigas/PremierLeague"
-        },
-        {
-            "id": 2,
-            "nome": "La liga",
-            "imagem":"imgsLigas/LaLiga"
-        },
-        {
-            "id": 3,
-            "nome":"Serie A",
-            "imagem": "imgsLigas/SerieA"
-        },
-        {
-            "id": 4,
-            "nome": "Bundesliga",
-            "imagem": "imgsLigas/Bundesliga"
-        },
-        {
-            "id": 5,
-            "nome": "Brasileirao",
-            "imagem": "imgsLigas/Brasileirao"
-        }
-    ]
-}
+const ligas = [
+  {
+    id: 1,
+    nome: "Premier League",
+    imagem: "imgsLigas/PremierLeague.png",
 
- const timesPrincipais = 
- {
-    "times":[
-        {
-        "id": "Time 1",
-        "nome": "Manchester United",
-        "imagem":"imgsTimes/ManchesterUnited"
-        }
-    ]
- }
+    times: [
+      {
+        id: 1,
+        nome: "Liverpool",
+        imagem: "imgsTimes/Liverpool.png",
+      },
+      {
+        id: 2,
+        nome: "Manchester United",
+        imagem: "imgsTimes/ManchesterUnited.png",
+      },
+      {
+        id: 3,
+        nome: "Arsenal",
+        imagem: "imgsTimes/Arsenal.png",
+      },
+      {
+        id: 4,
+        nome: "Manchester City",
+        imagem: "imgsTimes/City.png",
+      },
+      {
+        id: 5,
+        nome: "Chelsea",
+        imagem: "imgsTimes/Chelsea.png",
+      },
+      {
+        id: 6,
+        nome: "Tottenham",
+        imagem: "imgsTimes/Tottenham.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: "La liga",
+    imagem: "imgsLigas/LaLiga.png",
+
+    times: [
+      {
+        id: 1,
+        nome: "Real Madrid",
+        imagem: "imgsTimes/RealMadrid.png",
+      },
+      {
+        id: 2,
+        nome: "Barcelona",
+        imagem: "imgsTimes/Barcelona.png",
+      },
+      {
+        id: 3,
+        nome: "Atletico de Madrid",
+        imagem: "imgsTimes/AtleticoMadrid.png",
+      },
+      {
+        id: 4,
+        nome: "Athletic Bilbao",
+        imagem: "imgsTimes/Bilbao.png",
+      },
+      {
+        id: 5,
+        nome: "Valencia",
+        imagem: "imgsTimes/Valencia.png",
+      },
+      {
+        id: 6,
+        nome: "Real Sociedad",
+        imagem: "imgsTimes/Sociedad.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    nome: "Serie A",
+    imagem: "imgsLigas/SerieA.png",
+
+    times: [
+      {
+        id: 1,
+        nome: "Milan",
+        imagem: "imgsTimes/Milan.png",
+      },
+      {
+        id: 2,
+        nome: "Juventus",
+        imagem: "imgsTimes/Juventus.png",
+      },
+      {
+        id: 3,
+        nome: "Inter de Milão",
+        imagem: "imgsTimes/Inter.png",
+      },
+      {
+        id: 4,
+        nome: "Roma",
+        imagem: "imgsTimes/Roma.png",
+      },
+      {
+        id: 5,
+        nome: "Napoli",
+        imagem: "imgsTimes/Napoli.png",
+      },
+      {
+        id: 6,
+        nome: "Genoa",
+        imagem: "imgsTimes/Genoa.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    nome: "Bundesliga",
+    imagem: "imgsLigas/Bundesliga.png",
+
+    times: [
+      {
+        id: 1,
+        nome: "Bayern de Munique",
+        imagem: "imgsTimes/Bayern.png",
+      },
+      {
+        id: 2,
+        nome: "Borussia Dortmund",
+        imagem: "imgsTimes/Borussia.png",
+      },
+      {
+        id: 3,
+        nome: "Nuremberg",
+        imagem: "imgsTimes/Nuremberg.png",
+      },
+      {
+        id: 4,
+        nome: "Schalke 04",
+        imagem: "imgsTimes/Schalke.png",
+      },
+      {
+        id: 5,
+        nome: "Hamburgo",
+        imagem: "imgsTimes/Hamburgo.png",
+      },
+      {
+        id: 6,
+        nome: "Stuttgart",
+        imagem: "imgsTimes/Stuttgart.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    nome: "Brasileirao",
+    imagem: "imgsLigas/Brasileirao.png",
+
+    times: [
+      {
+        id: 1,
+        nome: "Flamengo",
+        imagem: "imgsTimes/Flamengo.png",
+      },
+      {
+        id: 2,
+        nome: "Palmeiras",
+        imagem: "imgsTimes/Palmeiras.png",
+      },
+      {
+        id: 3,
+        nome: "Santos",
+        imagem: "imgsTimes/Santos.png",
+      },
+      {
+        id: 4,
+        nome: "São Paulo",
+        imagem: "imgsTimes/SãoPaulo.png",
+      },
+      {
+        id: 5,
+        nome: "Atlético Mineiro",
+        imagem: "imgsTimes/Galo.png",
+      },
+      {
+        id: 6,
+        nome: "Cruzeiro",
+        imagem: "imgsTimes/Cruzeiro.png",
+      },
+    ],
+  },
+];
