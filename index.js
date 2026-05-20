9554b395d589f060188c2a4a563168402ad2de99
Henrique Shevchenko da Cunha Lopes Ferreira <1651397@sga.pucminas.br>
2026-05-20 10:31:14 -0300

    Inicio do Projeto

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+const ligas =
+{
+    "imagens":[
+        {
+            "id": 1,
+            "nome": "Premier League",
+            "imagem": "imgsLigas/PremierLeague"
+        },
+        {
+            "id": 2,
+            "nome": "La liga",
+            "imagem":"imgsLigas/LaLiga"
+        },
+        {
+            "id": 3,
+            "nome":"Serie A",
+            "imagem": "imgsLigas/SerieA"
+        },
+        {
+            "id": 4,
+            "nome": "Bundesliga",
+            "imagem": "imgsLigas/Bundesliga"
+        },
+        {
+            "id": 5,
+            "nome": "Brasileirao",
+            "imagem": "imgsLigas/Brasileirao"
+        }
+    ]
+}
 
+ const timesPrincipais = 
+ {
+    "times":[
+        {
+        "id": "Time 1",
+        "nome": "Manchester United",
+        "imagem":"imgsTimes/ManchesterUnited"
+        }
+    ]
+ }
